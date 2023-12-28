719d6239537778e5a776f5d82618759ef0cf7093
patelam2211 <patelakash221199@gmail.com>
2023-12-28 21:47:16 +0530

    newStructure

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+import Image from 'next/image'
+import Slider from '@/components/slider/Slider'
+export default function Home() {
+  return (
+     <div>
+        <h1>Hello</h1>
+     </div>
+  )
+}
